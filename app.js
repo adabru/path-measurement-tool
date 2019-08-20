ca1b2cb18e86dd2ef07939a918df23056098c0ea
adabru <b.brunnmeier@gmx.de>
2019-08-20 20:09:07 +0200

    feedback integration

--- app.js
+++ app.js
@@ -27,13 +27,16 @@ function update() {
   canvas.width = canvas.clientWidth
   canvas.height = canvas.clientHeight
   let colors = [['#060', '#0f0'], ['#600', '#f00']]
-  let nodeColors = ['#0f03', '#f003']
+  let nodeColors = ['#afaa', '#faaa']
   let ctx = canvas.getContext('2d');
+  ctx.translate(panX, panY)
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  ctx.fillStyle = '#1234';
-  ctx.fillRect(0, 0, 2000, 2000);
+  // background
+  ctx.clearRect(-5000, -5000, 10000, 10000)
+  ctx.fillStyle = '#1234'
+  ctx.fillRect(-5000, -5000, 10000, 10000)
 
+  // path lines
   for (let i = 0; i < g.length; i++) {
     for (let j = 1; j < g[i].length; j++) {
       ctx.beginPath()
@@ -44,13 +47,16 @@ function update() {
     }
   }
 
-  ctx.lineWidth = 0;
+  // node circles
+  ctx.lineWidth = 2
+  ctx.strokeStyle = '#000'
   for (let i = 0; i < g.length; i++) {
     for (let j = 0; j < g[i].length; j++) {
       ctx.fillStyle = nodeColors[i % colors.length]
       ctx.beginPath()
-      ctx.arc(...g[i][j], 5, 0, 2 * Math.PI, false);
+      ctx.arc(...g[i][j], 5, 0, 2 * Math.PI, false)
       ctx.fill()
+      ctx.stroke()
     }
   }
 
@@ -61,11 +67,39 @@ document.querySelector('#close').addEventListener('click', () => {
   w.close()
 })
 
-canvas.addEventListener('mousedown', ({clientX, clientY}) => {
-  if (g.length == 0)
-    g.push([])
-  g[g.length - 1].push([clientX - canvas.offsetLeft, clientY - canvas.offsetTop])
-  update()
+var panStartX, panStartY
+var panX = 0, panY = 0
+canvas.addEventListener('mousedown', (e) => {
+  if (e.button == 0) {
+    if (g.length == 0)
+      g.push([])
+    g[g.length - 1].push([e.clientX - canvas.offsetLeft - panX, e.clientY - canvas.offsetTop - panY])
+    update()
+  } else if (e.button == 1) {
+    panStartX = panX - e.clientX
+    panStartY = panY - e.clientY
+  }
+})
+canvas.addEventListener('mousemove', (e) => {
+  if (e.buttons == 2) {
+    // move nearest point
+    let dist2 = ([x1, y1], [x2, y2]) => (x1-x2)**2 + (y1-y2)**2
+    let cursor = [e.clientX - canvas.offsetLeft - panX, e.clientY - canvas.offsetTop - panY]
+    let nearest = [-100,-100]
+    for (let path of g)
+      for (let node of path) 
+        if (dist2(node, cursor) < dist2(nearest, cursor))
+          nearest = node
+    if (dist2(nearest, cursor) < 50**2) {
+      nearest[0] = cursor[0]
+      nearest[1] = cursor[1]
+      update()  
+    }
+  } else if (e.buttons == 4) {
+    panX = panStartX + e.clientX
+    panY = panStartY + e.clientY
+    update()
+  }
 })
 
 input.addEventListener('input', update)
